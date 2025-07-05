de799942f7f464ccc997639efe02fa92f67a0db8
Lis Pacheco <lmpacheco@uce.edu.ec>
2025-07-05 11:44:13 -0500

    fix: correct test by seeding mock data and removing deprecated MongoDB options

--- tests/status.test.js
+++ tests/status.test.js
@@ -2,9 +2,22 @@ const request = require('supertest');
 const app = require('../app');
 const connectDB = require('../config/db');
 const mongoose = require('mongoose');
+const AdoptionStatus = require('../models/status.model');
 
 beforeAll(async () => {
   await connectDB();
+});
+
+beforeEach(async () => {
+  await AdoptionStatus.create({
+    petId: 'pet123',
+    status: 'reserved',
+    notes: 'Test pet'
+  });
+});
+
+afterEach(async () => {
+  await AdoptionStatus.deleteMany({});
 });
 
 afterAll(async () => {
